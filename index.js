377b2c5360ece09ffb1175add9811fab02de9f0e
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-04 09:27:01 -0700

    Step 3 - Verify Axios is working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 
-const setup = () => {
-  console.log("Hello World!");
+const setup = async () => {
+  // test out poke api using axios here
+  const res = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+  console.log(res.data);
 }
 
 
