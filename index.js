3a1da5a43c85fb328baca7e75faebdd4808acee9
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-04 10:16:22 -0700

    Step 4.4 Pokemons Grid - Display the pokemon's details in the modal

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const setup = async () => {
   pokemons.forEach(async (pokemon) => {
     const res = await axios.get(pokemon.url)
     $('#pokeCards').append(`
-      <div class="pokeCard card"  >
+      <div class="pokeCard card" pokeName=${res.data.name}  >
         <h3>${res.data.name.toUpperCase()}</h3> 
         <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
@@ -19,6 +19,48 @@ const setup = async () => {
         </div>  
         `)
   })
+
+
+
+  // pop up modal when clicking on a pokemon card
+  // add event listener to each pokemon card
+  $('body').on('click', '.pokeCard', async function (e) {
+    const pokemonName = $(this).attr('pokeName')
+    // console.log("pokemonName: ", pokemonName);
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    // console.log("res.data: ", res.data);
+    const types = res.data.types.map((type) => type.type.name)
+    // console.log("types: ", types);
+    $('.modal-body').html(`
+        <div style="width:200px">
+        <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
+        <div>
+        <h3>Abilities</h3>
+        <ul>
+        ${res.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`).join('')}
+        </ul>
+        </div>
+
+        <div>
+        <h3>Stats</h3>
+        <ul>
+        ${res.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+        </ul>
+
+        </div>
+
+        </div>
+          <h3>Types</h3>
+          <ul>
+          ${types.map((type) => `<li>${type}</li>`).join('')}
+          </ul>
+      
+        `)
+    $('.modal-title').html(`
+        <h2>${res.data.name.toUpperCase()}</h2>
+        `)
+  })
+
 }
 
 
