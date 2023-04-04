cf2ed82647762a2d24ddcc9df4798ea1899128fe
Nabil M. Al-Rousan <rousani@gmail.com>
2023-04-04 09:37:06 -0700

    Step 4.1 Pokemons Grid - Fetch all pokemons names from the API

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 
 const setup = async () => {
   // test out poke api using axios here
-  const res = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
-  console.log(res.data);
+
+
+  $('#pokeCards').empty()
+  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+  const pokemons = response.data.results;
+  pokemons.forEach(async (pokemon) => {
+    const res = await axios.get(pokemon.url)
+    $('#pokeCards').append(`
+        ${res.data.name.toUpperCase()} 
+        `)
+  })
 }
 
 
